72c89969ab78796772371387094b7c2085401bda
mubarak23 <mubarakaminu340@gmail.com>
2025-05-06 15:58:21 +0100

    chore: refactor check for valid signature

--- src/validator.rs
+++ src/validator.rs
@@ -1,10 +1,10 @@
 use crate::types::TokenLockedEvent;
 use ethers::types::Signature;
-use ethers::types::Signature;
+
 
 pub fn is_signature_valid(event: &TokenLockedEvent) -> anyhow::Result<bool> {
-  let expected_signer = "0x...".parse().unwrap();
-  let msg_hash = ethers::utils::keccak256(format!("{:?}{:?}", event.sender, event.amount));
-  let signature: Signature = event.signature.unwrap()
+  let expected_signer = "0x...".parse().unwrap(); // to be replaced with an actual Ethereum address (like "0x123abc...").
+  let msg_hash = ethers::utils::keccak256(format!("{:?}{:?}", event.sender, event.amount)); // will optimize using ABI-encoding
+  let signature: Signature = event.signature.clone().ok_or_else(||, anyhow::anyhow!("Missing signature in TokenLockedEvent"))?
   Ok(signature.recover(msg_hash)?.eq(&expected_signer))
 }
