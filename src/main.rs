ab86ae73fb34b2ee7558a81842139ce47b37b7a6
mubarak23 <mubarakaminu340@gmail.com>
2025-05-08 15:01:00 +0100

    feat:main function to listen for event and dispatch them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod storage;
+mod event_listener;
+mod types;
+mod relayer;
+mod validator;
+
+
+#[tokio::main]
+asyn fn main() -> anyhow::Result<()> {
+    let config = load_config();
+    let (tx, mut rx) = mpsc::channel(100); // channel buffer capacity set at 100, but can be change to suit need
+    let signer = config.private_key.parse::<LocalWallet>()?.with_chain_id(137) // chain id of polygon is user, this can be configurable 
+    let provider = Provider::<http>::try_from(config.dest_rpc_url.clone())?;
+    let client = Arc::new(SignerMiddleware::new(provider, signer));
+
+    tokio::spawn(event_listener::listen_for_events(&config, tx));
+
+    while let Some(event) = rs.recv().await {
+        if let Err(err) = relayer::relay(event, &config, client.clone()).await {
+            eprintln!("Relay error: {:?}", err); // there could be a better way to handle this
+        }
+    }
+
+    Ok(())
 }
